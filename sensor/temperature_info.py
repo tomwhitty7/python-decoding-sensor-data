adf6c9bc8a8203cd04630f62d955ebc66aefc494
Tom Whitty <tomwhitty7@gmail.com>
2022-07-03 18:13:58 -0500

    f

--- sensor/temperature_info.py
+++ sensor/temperature_info.py
@@ -1,3 +1,4 @@
+from datetime import date, datetime
 from house_info import HouseInfo
 
 class TemperatureData(HouseInfo):
@@ -8,6 +9,9 @@ class TemperatureData(HouseInfo):
         return recs
 
     def get_data_by_area(self, rec_area=0):
-        recs = super.get_data_by_area()
-        recs.get_data_by_area("temperature")
-        return _convert_data(recs)
+        recs = super().get_data_by_area("temperature", rec_area)
+        return self._convert_data(recs)
+
+    def get_data_by_date(self, rec_date=date.today()):
+        recs = super().get_data_by_date("temperature", rec_date)
+        return self._convert_data(recs)
