420709b355c742a0d108abba692784ede3f80e74
Tom Whitty <tomwhitty7@gmail.com>
2022-07-03 18:01:16 -0500

    Update temperature_info.py

---
+++ sensor/temperature_info.py
@@ -0,0 +1,13 @@
+from house_info import HouseInfo
+
+class TemperatureData(HouseInfo):
+    def _convert_data(self, data):
+        recs = []
+        for rec in data:
+            recs.append(int(rec, base=10))
+        return recs
+
+    def get_data_by_area(self, rec_area=0):
+        recs = super.get_data_by_area()
+        recs.get_data_by_area("temperature")
+        return _convert_data(recs)
